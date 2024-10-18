e3734faf11bdcf40a7d8538e51d989588a660a0d
Amulya1609 <133862067+Amulya1609@users.noreply.github.com>
2024-10-18 17:27:35 +0530

    Add files via upload

---
+++ TechshopApp/TechshopApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MianModule;
+using DAO;
+
+namespace TechShop
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TechshopMenu menu = new TechshopMenu();
+            menu.DisplayMenu();
+
+
+        }
+
+    }
+}
---
+++ TechshopApp/Util/DBPropertyUtil.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace Util
+{
+    public class DBPropertyUtil
+    {
+        private static IConfigurationRoot _configuration;
+        static string s = null;
+        static DBPropertyUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("C:\\Users\\Amulya\\Desktop\\Hexaware\\Assignment\\Techshop_Sol\\Util\\appsettings.json",
+               optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+        public static string ReturnCn(string key)
+        {
+
+            s = _configuration.GetConnectionString("CnStr");
+
+            return s;
+        }
+    }
+}
